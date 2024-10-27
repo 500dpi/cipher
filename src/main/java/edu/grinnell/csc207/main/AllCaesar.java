c30650804c39f1c24f660e9680b726fedbacd0cf
Sara_Jaljaa <jaljaasa@grinnell.edu>
2024-10-26 19:15:20 -0500

    + clarity and descriptions

--- src/main/java/edu/grinnell/csc207/main/AllCaesar.java
+++ src/main/java/edu/grinnell/csc207/main/AllCaesar.java
@@ -1,69 +1,79 @@
-/*
- * AllCaesar.java
- *
- * Author: Sara Jaljaa
- * Course: CSC-207-01
- *
- */
-
 package edu.grinnell.csc207.main;
 
 import edu.grinnell.csc207.util.CipherUtils;
 import java.io.PrintWriter;
 
 /**
- * Prints and encrypted or decrypted string from a-z using the
- * Caesar cipher.
+ * Prints all the possible combinations (a-z) of an encrypted or
+ * decrypted Caesar cipher.
  *
  * @author Sara Jaljaa
- *
+ * @course CSC-207-01
  */
 public class AllCaesar {
 
+  // +----------------+----------------------------------------------
+  // | Helper Methods |
+  // +----------------+
+
   /**
-   * Prints a caesar cipher.
+   * Print all possible Caesar ciphers for a message.
    *
-   * @param str Takes a string to encipher.
-   * @param pen Takes an object to print from.
-   * @param choice Takes a letter, 'e' or 'd' indicating the action.
+   * @param str
+   *    The string to encipher.
+   * @param pen
+   *    The object to print from.
+   * @param choice
+   *    A letter (e or d) to indicate the cipher action.
    */
   public static void printCipher(String str, PrintWriter pen, char choice) {
-    for (char ch = 'a'; ch < (char) (('z') + 1); ch++) {
-      if (choice == 'e') {
-        pen.printf("n = %c: %s\n", ch, CipherUtils.caesarEncrypt(str, ch));
-      } else if (choice == 'd') {
-        pen.printf("n = %c: %s\n", ch, CipherUtils.caesarDecrypt(str, ch));
-      } else {
-        break;
-      } // if
+    for (char key = 'a'; key < (char) (('z') + 1); key++) {
+      switch (choice) {
+        case 'e':
+          pen.printf("n = %c: %s\n", key, CipherUtils.caesarEncrypt(str, key));
+          break;
+        case 'd':
+          pen.printf("n = %c: %s\n", key, CipherUtils.caesarDecrypt(str, key));
+          break;
+        default:
+          System.err.println("Error: Invalid cipher action.");
+          return;
+      } // switch
     } // for
   } // printCipher(String, PrintWriter, char)
 
+  // +-------------+-------------------------------------------------
+  // | Main Method |
+  // +-------------+
+
   /**
-   * Prints a caesar cipher from command line arguments.
+   * Prints a Caesar cipher from command line arguments.
    *
-   * @param args Command line arguments, where args[0] is the string
-   * and args[1] is the key.
+   * @param args
+   *    Command line arguments, where args[0] is cipher action and
+   *    args[1] is the message.
    */
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
 
-    String encode = "encode";
-    String decode = "decode";
-
-    if (args.length == 2) {
-      if (encode.equals(args[0])) {
-        printCipher(args[1], pen, 'e');
-      } else if (decode.equals(args[0])) {
-        printCipher(args[1], pen, 'd');
-      } else {
-        pen.close();
-        System.err.printf("Error: Invalid option: %s. Valid options are"
-                          + " encode\" or \"decode\".\n", args[0]);
-      } // if
-    } else {
-      System.err.println("Error: Incorrect number of parameters.");
+    if (args.length != 2) {
+      System.err.printf("Error: Incorrect number of parameters (%d).\n", args.length);
+      return;
     } // if
+
+    if (!(CipherUtils.lowercaseCheck(args[0]))) {
+      System.err.println("Error: Strings must be only lowercase letters.");
+      return;
+    } // if
+
+    if (args[0].equals("encode")) {
+      printCipher(args[1], pen, 'e');
+    } else if (args[0].equals("decode")) {
+      printCipher(args[1], pen, 'd');
+    } else {
+      System.err.printf("Error: Invalid option: %s. Valid options are"
+                        + " \"encode\" or \"decode\".\n", args[0]);
+    } // elif
   } // main(String[])
 } // class main
 
