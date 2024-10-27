78a7bcc055c1b32ae08cfceb5bda08e991519b8d
Sara_Jaljaa <jaljaasa@grinnell.edu>
2024-10-26 19:16:28 -0500

    +/- organization, descriptions, helper methods

--- src/main/java/edu/grinnell/csc207/main/Cipher.java
+++ src/main/java/edu/grinnell/csc207/main/Cipher.java
@@ -1,59 +1,82 @@
-/*
- * Cipher.java
- *
- * Author: Sara Jaljaa
- * Course: CSC-207-01
- *
- */
-
 package edu.grinnell.csc207.main;
 
 import edu.grinnell.csc207.util.CipherUtils;
 import java.io.PrintWriter;
 
 /**
- * Prints encrypted or decrypted Vigenere/Caesar ciphers.
+ * Prints an encrypted or decrypted Vigenere/Caesar cipher with
+ * the specified input values.
  *
  * @author Sara Jaljaa
+ * @course CSC-207-01
  */
 public class Cipher {
 
-  /**
-   * Compares a val to two strings + returns a boolean depending on
-   * if the string matches one of the values and has an empty
-   * String to later store the matching value.
-   *
-   * @param compareVal The string to compare to others.
-   * @param str1 The first string to compare with.
-   * @param str2 The second string to compare with.
-   * @param store An empty String variable.
-   * @return True if the strings are all the same and store is empty (null), else
-   *         false.
-   */
-  public static boolean compareArg(String compareVal, String str1, String str2, String store) {
-    return ((compareVal.equals(str1) || compareVal.equals(str2)) && store == null);
-  } // compareArg(String, String, String, String)
+  // +-----------+---------------------------------------------------
+  // | Constants |
+  // +-----------+
 
   /**
    * Required parameters to create a cipher.
    */
   private static final int PARAMS = 4;
 
+  // +----------------+----------------------------------------------
+  // | Helper Methods |
+  // +----------------+
+
+  /**
+   * Checks if either strings match the desired value.
+   *
+   * @param match
+   *    The string to compare to.
+   * @param str1
+   *    The first string to compare.
+   * @param str2
+   *    The second string to compare.
+   *
+   * @return
+   *    True if one of the strings are the same as the string to match.
+   */
+  public static boolean matchCheck(String match, String str1, String str2) {
+    if (emptyCheck(match)) {
+      return false;
+    } // if
+    return (match.equals(str1) || match.equals(str2));
+  } // matchCheck(String, String, String, String)
+
+  /**
+   * Checks if a string is empty.
+   *
+   * @param arg
+   *    The string to check.
+   *
+   * @return
+   *    True or false if the string is empty.
+   */
+  public static boolean emptyCheck(String arg) {
+    return ((arg == null) || arg.equals(""));
+  } // emptyCheck(String)
+
+  // +-------------+-------------------------------------------------
+  // | Main Method |
+  // +-------------+
+
   /**
    * Creates a cipher based on command line arguments.
    *
-   * @param args Command line arguments to encode/decode,
-   * the type of cipher (vigenere/caesar), the input string, and key.
+   * @param args
+   *    Command line arguments that indicate the type, action,
+   *    message, and key (in any order) to create a cipher.
    */
-  @SuppressWarnings("unused")
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
 
-    // Variables to store parameters to cipher
+    // Empty parameters
+    String key = null;
     String type = null;
     String action = null;
-    String target = null;
-    String key = null;
+    String message = null;
 
     if (args.length != PARAMS) {
       System.err.println("Error: Expected 4 parameters, received: " + args.length);
@@ -61,66 +84,71 @@ public class Cipher {
     } // if
 
     for (int i = 0; i < args.length; i++) {
-      if (compareArg(args[i], "-vigenere", "-caesar", type)) {
+      // Check for the cipher type
+      if (matchCheck(args[i], "-vigenere", "-caesar") && type == null) {
         type = args[i];
-      } else if (compareArg(args[i], "-encode", "-decode", action)) {
+      // Check for the cipher action
+      } else if (matchCheck(args[i], "-encode", "-decode") && action == null) {
         action = args[i];
-      } else if (target == null) {
-        if (CipherUtils.allLower(args[i])) {
-          target = args[i];
+      // Check for the plaintext
+      } else if (message == null) {
+        if (emptyCheck(args[i])) {
+          return;
+        } else if (CipherUtils.lowercaseCheck(args[i])) {
+          message = args[i];
         } else {
-          System.err.println("Error: Strings must be only lowercase letters.");
-          break;
-        } // if/else
+          System.err.println("Error: String must be only lowercase letters.");
+          return;
+        } // elif
+      // Check for the cipher key
       } else if (key == null) {
-        if (CipherUtils.allLower(args[i])) {
+        if (emptyCheck(args[i])) {
+          System.err.println("Error: Empty keys are not permitted.");
+          return;
+        } else if (CipherUtils.lowercaseCheck(args[i])) {
           key = args[i];
         } else {
-          System.err.println("Error: Keys must be only lowercase letters.");
-          break;
-        } // if/else
-      } // if
+          System.err.println("Error: Key must be only lowercase letters.");
+          return;
+        } // elif
+      } // elif
     } // for
 
-    // Check for presence of cipher type and action specified in arguments.
-    if (action == null) {
+    // Check that the cipher action and type have valid values
+    if (action.equals("")) {
       System.err.println("Error: no valid action specified."
-                        + "Legal values are \'-encode\' and \'-decode\'");
+                        + "Legal values are \"-encode\" and \"-decode\"");
       return;
-    } else if (type == null) {
+    } else if (type.equals("")) {
       System.err.println("Error: no valid cipher specified."
-                        + "Legal values are \'-caesar\' and \'-vigenere\'");
+                        + "Legal values are \"-caesar\" and \"-vigenere\"");
       return;
-    } else if (key == null) {
-      System.err.println("Error: Empty keys are not permitted.");
-      return;
-    } // if
+    } // elif
 
-    // Check the caesar cipher has a one-character key.
-    if ((type.equals("-caesar")) && (key.length() > 1)) {
+    // If the type is a Caesar cipher, check that the key is the proper length
+    if ((type.equals("-caesar")) && (key.length() != 1)) {
       System.err.println("Error: Caesar ciphers require a one-character key.");
       return;
     } // if
 
-    // Print the cipher depending on its type & action.
+    // Print the cipher
     switch (type) {
       case "-vigenere":
         if (action.equals("-decode")) {
-          pen.println(CipherUtils.vigenereDecrypt(target, key));
+          pen.println(CipherUtils.vigenereDecrypt(message, key));
         } else if (action.equals("-encode")) {
-          pen.println(CipherUtils.vigenereEncrypt(target, key));
-        } // if
+          pen.println(CipherUtils.vigenereEncrypt(message, key));
+        } // elif
         break;
       case "-caesar":
         if (action.equals("-decode")) {
-          pen.println(CipherUtils.caesarDecrypt(target, key.charAt(0)));
+          pen.println(CipherUtils.caesarDecrypt(message, key.charAt(0)));
         } else if (action.equals("-encode")) {
-          pen.println(CipherUtils.caesarEncrypt(target, key.charAt(0)));
-        } // if
+          pen.println(CipherUtils.caesarEncrypt(message, key.charAt(0)));
+        } // elif
         break;
       default:
         break;
     } // switch
-    pen.close();
   } // main(String[])
 } // class Cipher
